19e9691979810b02fe815c366fa93020aa2b9612
Mariam Tokarski <mariamtokarski@gmail.com>
2018-01-23 20:33:37 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,6 @@
+var recipies = {oats:"2 cups", butter:"1 cup", sugar:"1.5 cups", milk:"half cup", cocoa:"5 tbsp", vanilla:"2 tsp"}
+
+function updateObjectWithKeyAndValue(recipies, prop, amount){
+  recipies.prop = "1"
+  return recipies
+}
